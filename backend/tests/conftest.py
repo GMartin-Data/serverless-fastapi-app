40b87d8a3c9f4f9951789ffff2646b894d243248
Greg <gregory.martin.data@gmail.com>
2025-05-30 15:51:24 +0200

    refactor(tests): reset in-memory db for each test in async_client fixture
    Modifies the async_client fixture to clear fake_items_db and reset item_id_counter before each test, ensuring test isolation for in-memory data.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,14 +4,22 @@ import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 
 from backend.app.main import app as fastapi_app
+from backend.app.main import fake_items_db, item_id_counter
 
 
 @pytest_asyncio.fixture
 async def async_client() -> AsyncIterator[AsyncClient]:
     """
     Yield an `AsyncClient` instance, configured to make requests directly to
-    the in-memory FastAPI 'fast
+    the in-memory FastAPI 'fastapi_app'.
+    Resets the in-memory database before each test.
     """
+    # Reseting the in-memory database and counter ensure test isolation for our
+    # in-memory data
+    global item_id_counter
+    fake_items_db.clear()
+    item_id_counter = 0
+
     async with ASGITransport(app=fastapi_app) as transport:
         async with AsyncClient(
             transport=transport, base_url="http://testserver"
